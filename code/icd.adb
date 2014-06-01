d03307be71e1449ce3e57db5138b35a1152a251d
Saman Abdolmohammadpour <samanamp@outlook.com>
2014-06-01 19:53:38 +1000

    working on contracts

--- code/icd.adb
+++ code/icd.adb
@@ -1,22 +1,59 @@
 with Measures;
 with HRM;
 with ImpulseGenerator;
-with Ada.Text_IO;
+--with Ada.Text_IO;
 
 package body ICD is
 
    NoShock : constant Measures.Joules := 0;
 
-   procedure Off(Computer: in out ICDType) is
+   procedure Init(Computer : out ICDType) is
    begin
-      Computer.IsOn := False;
-   end Off;
+        Computer.InProcess := False;
+        Computer.IsOn := False;
+
+        Computer.UpperBound := 130;
+        Computer.Next := 600 / (Computer.UpperBound + ProjectedRate);
+        
+        Computer.history_avarage := 0;
+        Computer.history_variance := 0;
+
+        Computer.state := normal;
+
+        
+        Computer.TickCount := 0;
+        Computer.Count := 0;
+
+        Computer.Rate := Measures.BPM'First;
+        Computer.heartRateHistory1 := Measures.BPM'First;
+        Computer.heartRateHistory2 := Measures.BPM'First;
+        Computer.heartRateHistory3 := Measures.BPM'First;
+        Computer.heartRateHistory4 := Measures.BPM'First;
+        Computer.heartRateHistory5 := Measures.BPM'First;
+        Computer.heartRateHistory6 := Measures.BPM'First;
+        Computer.ticksToReEnableDetectionAgain := 6;
+   end Init;
 
    procedure On(Computer: in out ICDType) is
    begin
       Computer.IsOn := True;
    end On;
 
+   procedure Off(Computer: in out ICDType) is
+   begin
+      Computer.IsOn := False;
+   end Off;
+
+   procedure addRateToHistory(Computer : in out ICDType) is
+   begin
+          Computer.heartRateHistory1 := Computer.heartRateHistory2;
+          Computer.heartRateHistory2 := Computer.heartRateHistory3;
+          Computer.heartRateHistory3 := Computer.heartRateHistory4;
+          Computer.heartRateHistory4 := Computer.heartRateHistory5;
+          Computer.heartRateHistory5 := Computer.heartRateHistory6;
+          Computer.heartRateHistory6 := Computer.rate;
+   end addRateToHistory;
+   
    procedure Detect_Tarchycardia(Computer : in out ICDType) is
    begin
        -- if the icd is on and the rate is above the upperbound
@@ -27,7 +64,6 @@ package body ICD is
           Computer.state := tar;
           Computer.Count := MaxShocks;
           Computer.InProcess := True;
-          Set_Next(Computer);
           Computer.TickCount := Computer.Next;
        end if;
 
@@ -44,12 +80,7 @@ package body ICD is
        end if;
    end Detect_Tarchycardia;
 
-   procedure Set_Next(Computer : in out ICDType) is
-   begin
-      if Computer.state = tar and Computer.Count > 0 and Computer.InProcess then
-         Computer.Next := Integer(Float'Floor (600.0 / (Float(Computer.UpperBound) + ProjectedRate)));
-      end if;
-   end Set_Next;
+   
 
    procedure Set_Impulse(Computer : in out ICDType; Shock: in out ImpulseGenerator.GeneratorType) is
    begin
@@ -60,7 +91,7 @@ package body ICD is
          ImpulseGenerator.SetImpulse(Shock, FibShock);
       elsif Computer.state = tar and Computer.InProcess and Computer.count > 0 
             and Computer.TickCount = 0 and Shock.IsOn then
-         Ada.Text_IO.Put_Line("SHOCK");
+         --Ada.Text_IO.Put_Line("SHOCK");
          ImpulseGenerator.SetImpulse(Shock, TarShock);
          Computer.count := Computer.count - 1;
          Computer.TickCount := Computer.Next;
@@ -70,83 +101,103 @@ package body ICD is
       end if;
    end Set_Impulse;
 
-   procedure Init(Computer : in out ICDType) is
+   procedure CalculateAvarage(Computer : in out ICDType) is
    begin
-        --Computer.IsTar := False;
-        --COmputer.IsFib := False;
 
-        Computer.state := normal;
+      Computer.history_avarage := (((((Computer.heartRateHistory1 +
+                                  Computer.heartRateHistory2) +
+                                  Computer.heartRateHistory3) +
+                                  Computer.heartRateHistory4) +
+                                  Computer.heartRateHistory5) +
+                                  Computer.heartRateHistory6)/ 6;
 
-        Computer.UpperBound := 130;
-        Computer.TickCount := 0;
-        Computer.Count := 0;
+   end CalculateAvarage;
 
-        Computer.Rate := Measures.BPM'First;
-        for I in Index loop
-          Computer.heartRateHistory(I) := -1;
-        end loop;
-        Computer.ticksToReEnableDetectionAgain := Computer.heartRateHistory'length;
-   end Init;
-
-   procedure addRateToHistory(Computer : in out ICDType) is
-      lastIndex : constant Integer := Computer.heartRateHistory'last;
+   function power(num : in Integer) return Integer is
+   result : Integer;
    begin
-      for I in Integer range 1 .. lastIndex - 1 loop
-        Computer.heartRateHistory(I) := Computer.heartRateHistory (I+1);
-      end loop;
-      --Put the new value
-      Computer.heartRateHistory (lastIndex) := Computer.rate;
-   end addRateToHistory;
+      result := Integer'Last;
+      if num * num < Integer'Last and num * num > Integer'First then
+        result := num * num;
+      end if;
+      return result;
+   end power;
+
+   function secureAdd(num1 : in Integer; num2 : in Integer) return Integer is
+   result : Integer;
+   begin
+      result := Integer'Last;
+      if num1 + num2 <= Integer'Last and num1 + num2 >= Integer'First then
+        result := num1 + num2;
+      end if;
+      return result;
+    end secureAdd;
+
+   procedure CalculateVariance(Computer : in out ICDType) is
+   begin
+
+      ---Calculate Variance--------------------
+
+      Computer.history_variance := 0;
+      if (Computer.history_avarage - Computer.heartRateHistory1) <= Integer'Last and
+      (Computer.history_avarage - Computer.heartRateHistory1) >= Integer'First and
+      (Computer.history_avarage - Computer.heartRateHistory2) <= Integer'Last and
+      (Computer.history_avarage - Computer.heartRateHistory2) >= Integer'First and
+      (Computer.history_avarage - Computer.heartRateHistory3) <= Integer'Last and
+      (Computer.history_avarage - Computer.heartRateHistory3) >= Integer'First and
+      (Computer.history_avarage - Computer.heartRateHistory4) <= Integer'Last and
+      (Computer.history_avarage - Computer.heartRateHistory4) >= Integer'First and
+      (Computer.history_avarage - Computer.heartRateHistory5) <= Integer'Last and
+      (Computer.history_avarage - Computer.heartRateHistory5) >= Integer'First and
+      (Computer.history_avarage - Computer.heartRateHistory6) <= Integer'Last and
+      (Computer.history_avarage - Computer.heartRateHistory6) >= Integer'First then
+
+      Computer.history_variance := (secureAdd(secureAdd(secureAdd(secureAdd(secureAdd(power(Computer.history_avarage - Computer.heartRateHistory1), 
+      power(Computer.history_avarage - Computer.heartRateHistory2)), 
+      power(Computer.history_avarage - Computer.heartRateHistory3)), 
+      power(Computer.history_avarage - Computer.heartRateHistory4)), 
+      power(Computer.history_avarage - Computer.heartRateHistory5)), 
+      power(Computer.history_avarage - Computer.heartRateHistory6))) / 6;
+      end if;
+      ---End Calculating the variance----------
+   end CalculateVariance;
+
 
    procedure Detect_Fibrillation(Computer : in out ICDType) is
-   currentHistory : History;
-   temp : Integer;
    begin
 
-      ---Calculate the avarage-----------------
-      currentHistory := Computer.heartRateHistory;
-      Computer.history_avarage := 0;
-      for I in Index loop
-      Computer.history_avarage := Computer.history_avarage + currentHistory (I);
-      end loop;
-      Computer.history_avarage := Computer.history_avarage / currentHistory'length;
-      ---end calculating the average-----------
-      ---Calculate Variance--------------------
-      Computer.history_variance := 0;
-      for I in Index loop
-      temp := Computer.history_avarage - Computer.heartRateHistory(I);
-      Computer.history_variance := temp * temp + Computer.history_variance;
-      end loop;
-      Computer.history_variance := Computer.history_variance / currentHistory'length;
-      ---End Calculating the variance----------
+    if Computer.ticksToReEnableDetectionAgain > Integer'First
       if Computer.state = fib then
         --Ada.Text_IO.Put_Line("fib is true");
         Computer.state := normal;
         Computer.ticksToReEnableDetectionAgain := 7;
       else
-       if currentHistory (1) = -1 or 
+       if Computer.heartRateHistory1 = -1 or 
             Computer.ticksToReEnableDetectionAgain > 0 then
             --Ada.Text_IO.Put_Line("fib nodetect");
             Computer.ticksToReEnableDetectionAgain := 
             Computer.ticksToReEnableDetectionAgain - 1;
        else
-            If Computer.history_variance > 3000 then
+            If Computer.history_variance > 100 then
               --Ada.Text_IO.Put_Line(".");
               Computer.state := fib;
            end if;
        end if; 
     end if;
-
+    end if;
    end Detect_Fibrillation;
 
    procedure Tick(Computer : in out ICDType; HeartRateMonitor : in HRM.HRMType; Shock : in out ImpulseGenerator.GeneratorType) is
    begin
 
       if Computer.IsOn then
+            
         --Ada.Text_IO.Put_Line("Computer is on");
         -- read the heart rate from the HRM
         HRM.GetRate(HeartRateMonitor, Computer.Rate);
         addRateToHistory(Computer);
+        CalculateAvarage(Computer);
+        CalculateVariance(Computer);
         Detect_Fibrillation(Computer);
         Detect_Tarchycardia(Computer);
         Set_Impulse(Computer, Shock);
